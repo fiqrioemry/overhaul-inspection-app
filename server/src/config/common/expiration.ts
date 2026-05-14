1df0f7bbfb8bedc05cb276a05adabf6b6bedddd6
fiqrioemry <foemry@gmail.com>
2026-05-14 23:58:03 +0700

    remove common folder, replace to constant

--- server/src/config/common/expiration.ts
+++
@@ -1,5 +0,0 @@
-const expConfig = {
-  avatar: 1 * 60,
-};
-
-export default expConfig;
--- server/src/config/common/tokenLimit.ts
+++
@@ -1,7 +0,0 @@
-const tokenLimit = {
-  verifyEmail: 60 * 60 * 1000, // 60 minutes in milliseconds
-  sessionToken: 1000 * 60 * 60 * 24 * 7, // 7 days in milliseconds
-  passwordReset: 60 * 60 * 1000, // 60 minutes in milliseconds
-};
-
-export default tokenLimit;
