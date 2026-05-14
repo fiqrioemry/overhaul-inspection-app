d5dc6377e7d2791a1338ba1061475847a24a9a95
fiqrioemry <foemry@gmail.com>
2026-05-15 00:07:46 +0700

    feat(add): notification features

---
+++ server/src/repositories/notification.repository.ts
@@ -0,0 +1,111 @@
+import { Prisma } from "generated/prisma/edge";
+import { pgsql as database } from "@/config/database/pgsql";
+import { DeleteNotificationRequest, GetNotificationRequest, UpdateNotificationSettingRequest } from "@/schema/notification.validation";
+import { NotificationChannel, NotificationStatus, NotificationType } from "generated/prisma";
+
+export class NotificationRepository {
+  static async getNotificationsByUserId(query: GetNotificationRequest) {
+    const { userId, type, sortBy, orderBy, search, page = 1, limit = 10 } = query;
+    let where: any = {
+      userId: userId!,
+      description: { contains: search, mode: "insensitive" } as const,
+    };
+
+    if (type) {
+      where = { ...where, type };
+    }
+
+    const [notifications, totalItems] = await Promise.all([
+      database.notification.findMany({
+        where,
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          type: true,
+          metadata: true,
+          readAt: true,
+          createdAt: true,
+        },
+        orderBy: {
+          [orderBy!]: sortBy!,
+        },
+        skip: (Number(page) - 1) * Number(limit),
+        take: Number(limit),
+      }),
+      database.notification.count({ where }),
+    ]);
+
+    return { notifications, totalItems };
+  }
+  static async createNotification(tx: Prisma.TransactionClient, data: { userId: string; title: string; description: string; type: NotificationType; metadata: Record<string, any> }) {
+    const db = tx ?? database;
+    await db.notification.create({
+      data,
+    });
+  }
+
+  static async markAsRead(userId: string, notificationIds: string[], readAt: Date) {
+    await database.notification.updateMany({
+      where: {
+        id: { in: notificationIds },
+        userId,
+      },
+      data: { readAt },
+    });
+  }
+
+  static async createNotificationSettings(
+    tx: Prisma.TransactionClient,
+    data: {
+      userId: string;
+      type: NotificationType;
+      channel: NotificationChannel;
+      status: NotificationStatus;
+    }[],
+  ) {
+    const db = tx ?? database;
+    await db.notificationSetting.createMany({
+      data,
+      skipDuplicates: true,
+    });
+  }
+
+  static async getNotificationSettings(userId: string) {
+    return await database.notificationSetting.findMany({
+      where: { userId },
+      select: {
+        id: true,
+        type: true,
+        channel: true,
+        status: true,
+      },
+    });
+  }
+
+  static async getNotificationByType(userId: string, type: NotificationType) {
+    return await database.notificationSetting.findFirst({
+      where: { userId, type },
+      select: {
+        id: true,
+        userId: true,
+        status: true,
+      },
+    });
+  }
+
+  static async updateNotificationSettings(tx: Prisma.TransactionClient, payload: UpdateNotificationSettingRequest) {
+    const db = tx ?? database;
+    await db.notificationSetting.update({
+      where: { id: payload.notificationId },
+      data: { status: payload.status },
+    });
+  }
+
+  static async deleteNotification(tx: Prisma.TransactionClient, payload: DeleteNotificationRequest) {
+    const db = tx ?? database;
+    await db.notification.deleteMany({
+      where: { userId: payload.userId, id: { in: payload.notificationIds } },
+    });
+  }
+}
---
+++ server/src/services/notification.service.ts
@@ -0,0 +1,79 @@
+import { Context } from "hono";
+import { pgsql } from "@/config/database/pgsql";
+import { UserRepository } from "@/repositories/user.repository";
+import { notificationAction } from "@/config/constant/notification.constant";
+import { NotificationRepository } from "@/repositories/notification.repository";
+import { DeleteNotificationRequest, GetNotificationRequest, UpdateNotificationRequest, UpdateNotificationSettingRequest } from "@/schema/notification.validation";
+
+export class NotificationService {
+  static async getNotificationByUserId(c: Context, request: GetNotificationRequest) {
+    const { notifications, totalItems } = await NotificationRepository.getNotificationsByUserId(request);
+
+    const data = notifications.map((notification) => ({
+      id: notification.id,
+      title: notification.title,
+      description: notification.description,
+      type: notification.type,
+      metadata: notification.metadata,
+      readAt: notification.readAt,
+      createdAt: notification.createdAt,
+    }));
+
+    const meta = {
+      pagination: {
+        page: Number(request.page!),
+        limit: Number(request.limit!),
+        totalItems,
+        totalPages: totalItems > 0 ? Math.ceil(totalItems / Number(request.limit!)) : 0,
+      },
+      filter: {
+        search: request.search,
+        type: request.type,
+        orderBy: request.orderBy,
+        sortBy: request.sortBy,
+      },
+    };
+
+    return { data, meta };
+  }
+
+  static async markAsRead(c: Context, payload: UpdateNotificationRequest) {
+    await NotificationRepository.markAsRead(payload.userId!, payload.notificationIds, new Date());
+  }
+
+  static async getNotificationSettings(c: Context, userId: string) {
+    const notifications = await NotificationRepository.getNotificationSettings(userId);
+    return notifications.map((setting) => ({
+      id: setting.id,
+      type: setting.type,
+      channel: setting.channel,
+      status: setting.status,
+    }));
+  }
+
+  static async updateNotificationSettings(c: Context, payload: UpdateNotificationSettingRequest) {
+    await pgsql.$transaction(async (tx) => {
+      await NotificationRepository.updateNotificationSettings(tx, payload);
+
+      const userLogs = {
+        userId: payload.userId!,
+        action: notificationAction.UPDATE_NOTIFICATION_SETTINGS,
+        metadata: { status: payload.status },
+      };
+      await UserRepository.createActivityLog(tx, userLogs);
+    });
+  }
+
+  static async deleteNotification(c: Context, payload: DeleteNotificationRequest) {
+    await pgsql.$transaction(async (tx) => {
+      await NotificationRepository.deleteNotification(tx, payload);
+
+      const userLogs = {
+        userId: payload.userId!,
+        action: notificationAction.DELETE_NOTIFICATION,
+        metadata: { notificationIds: payload.notificationIds },
+      };
+      await UserRepository.createActivityLog(tx, userLogs);
+    });
+  }
+}
